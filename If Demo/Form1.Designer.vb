e4ea2b7ee9bc70f3f1959068dc2bcafe6b10e6cb
Clive B Harrison <clive_b_harrison@outlook.com>
2021-10-06 22:21:51 +0100

    Add project files.

---
+++ If Demo/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtValue = New System.Windows.Forms.TextBox()
+        Me.btnCheckNumbers = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(12, 68)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(75, 23)
+        Me.btnCheck.TabIndex = 0
+        Me.btnCheck.Text = "Check"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(12, 97)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 1
+        Me.txtName.Text = "Stephanie"
+        '
+        'txtValue
+        '
+        Me.txtValue.Location = New System.Drawing.Point(136, 96)
+        Me.txtValue.Name = "txtValue"
+        Me.txtValue.Size = New System.Drawing.Size(100, 20)
+        Me.txtValue.TabIndex = 2
+        '
+        'btnCheckNumbers
+        '
+        Me.btnCheckNumbers.Location = New System.Drawing.Point(136, 68)
+        Me.btnCheckNumbers.Name = "btnCheckNumbers"
+        Me.btnCheckNumbers.Size = New System.Drawing.Size(100, 23)
+        Me.btnCheckNumbers.TabIndex = 3
+        Me.btnCheckNumbers.Text = "Check Numbers"
+        Me.btnCheckNumbers.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 265)
+        Me.Controls.Add(Me.btnCheckNumbers)
+        Me.Controls.Add(Me.txtValue)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCheck As System.Windows.Forms.Button
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtValue As System.Windows.Forms.TextBox
+    Friend WithEvents btnCheckNumbers As System.Windows.Forms.Button
+
+End Class
---
+++ If Demo/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+
+    Private Sub btnCheck_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnCheck.Click
+
+        'Declare a variable and get the name from the text box
+        Dim strName As String
+        strName = txtName.Text
+
+        'Is the name Wendy?
+        If strName <> "Wendy" Then
+            MessageBox.Show("The name is *not* Wendy.", "If Demo")
+        End If
+
+    End Sub
+
+    Private Sub btnCheckNumbers_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnCheckNumbers.Click
+
+        'Declare variable
+        Dim intNumber As Integer
+
+        Try
+            'Get the number from the text box
+            intNumber = CType(txtValue.Text, Integer)
+        Catch
+            Exit Sub
+        End Try
+
+        'Is intNumber less than or equal to 27?
+        If intNumber <= 27 Then
+            MessageBox.Show("Is ‘intNumber’ less than or equal to 27? Yes!", _
+                "If Demo")
+        Else
+            MessageBox.Show("Is ‘intNumber’ less than or equal to 27? No!", _
+                "If Demo")
+
+        End If
+
+        'Is intNumber greater than 27?
+        If intNumber > 27 Then
+            MessageBox.Show("Is ‘intNumber’ greater than 27? Yes!", _
+                "If Demo")
+        Else
+            MessageBox.Show("Is ‘intNumber’ greater than 27? No!", _
+                "If Demo")
+        End If
+
+        'Is intNumber greater than or equal to 27?
+        If intNumber >= 27 Then
+            MessageBox.Show("Is ‘intNumber’ greater than or equal to 27? Yes!", _
+                "If Demo")
+        Else
+            MessageBox.Show("Is ‘intNumber’ greater than or equal to 27? No!", _
+                "If Demo")
+        End If
+
+    End Sub
+End Class
